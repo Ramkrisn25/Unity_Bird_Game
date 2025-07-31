36509ce79897f2966d514b9143db054ff2be298e
RAMKRISHNA MODAK <154114073+Ramkrisn25@users.noreply.github.com>
2025-07-31 23:21:21 +0530

    Add files via upload

---
+++ Assets/pipespawnersc.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class pipespawnersc : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnpipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawnpipe();
+            timer = 0;
+        }
+
+            
+    }
+    void spawnpipe()
+    {
+        float lowestpoint = transform.position.y - heightOffset;
+        float highestpoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range( lowestpoint , highestpoint),0), transform.rotation);
+
+    }
+}
